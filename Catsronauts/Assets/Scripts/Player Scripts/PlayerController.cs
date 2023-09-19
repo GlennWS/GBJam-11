4f2cd6a03b58daa165646fdf8aae1337641d8e0b
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2023-09-19 18:38:50 +0100

    Player can move between 5 seats with A + D.

--- Catsronauts/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Catsronauts/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public float moveSpeed = 5.0f;
+    public Transform[] seatPositions;
+    private int _currSeatIndex = 0;
+
+    private int CurrSeatIndex
     {
-        
+        get { return _currSeatIndex; }
+        set { _currSeatIndex = Mathf.Clamp(value, 0, 4); }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CurrSeatIndex--;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            CurrSeatIndex++;
+        }
+
+        Vector3 newPos = seatPositions[CurrSeatIndex].position;
+        newPos.y = transform.position.y;
+        transform.position = Vector3.MoveTowards(transform.position, newPos, moveSpeed * Time.deltaTime);
     }
 }
